92e61c26e275a88b632599785dbedefafc52168d
Birajit <birajit95@gmail.com>
2020-10-23 23:55:50 +0530

    Add: validate() is added and first name validation is done

--- userRegistration.sh
+++ userRegistration.sh
@@ -1,3 +1,20 @@
 #! /bin/bash -x
 
 echo "Welcome to User Registration Program"
+
+validate(){
+    local input=$1
+    local pat=$2
+
+    if [[ $input =~ $pat ]]
+    then
+        echo "Valid"
+    else
+        echo "Invalid"
+    fi   
+}
+
+
+read -p "Enter the first name : " firstName
+firstNamePattern='^[A-Z]{1}[a-zA-Z]{2,}$'
+validate $firstName $firstNamePattern
